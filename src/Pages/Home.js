d4dfe0351da8acbf3a5f2796d46e0aebd36a453a
Luke Son <flare2930@gmail.com>
2021-11-13 14:12:39 -0700

    css code for Home added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,15 +6,15 @@ import {
     Link
 } from "react-router-dom";
 import Main from './Main.js';
+import './Home.css';
 
 function Home() {
   return (
-    <div>
+    <div className="example">
         <h1>Welcome to Recipe Vault!</h1>
-
+        <img></img>
         {/* This is a button that will direct the user to the main page */}
         <Link to="main">Get Started </Link>
-        
     </div>
 
 
